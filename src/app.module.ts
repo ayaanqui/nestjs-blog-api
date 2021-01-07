a3f6cb5d317ab8d560308a7fec858cdf432222d2
ayaanqui <moahammedayaan.dev@gmail.com>
2021-01-07 12:44:04 -0600

    Configure MySQL with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { NerdController } from './controllers/nerd.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbconfig } from './database/dbconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: dbconfig.username,
+      password: dbconfig.password,
+      database: dbconfig.database,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, NerdController],
 })
-export class AppModule {}
+export class AppModule { }
