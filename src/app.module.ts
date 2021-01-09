73c5758233bd2fe0c89a2c8f058706223ed6a36d
ayaanqui <moahammedayaan.dev@gmail.com>
2021-01-08 22:01:25 -0600

    Use ormconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { NerdController } from './controllers/nerd.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dbconfig } from './database/dbconfig';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: dbconfig.username,
-      password: dbconfig.password,
-      database: dbconfig.database,
-      entities: [],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(),
   ],
   controllers: [AppController, NerdController],
 })
